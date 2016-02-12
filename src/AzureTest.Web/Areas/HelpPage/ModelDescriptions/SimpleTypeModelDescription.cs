785d6a1b820f761075baa986b1ec7b50db8d544b
Daniel Lillja <daniel.lillja@gmail.com>
2016-02-12 14:56:38 -0600

    Add web project

---
+++ src/AzureTest.Web/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AzureTest.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ src/AzureTest.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AzureTest.Web.WebApiApplication" Language="C#" %>
